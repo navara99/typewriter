a885e4e3e310f1258174d20b1cfd80885d7e984d
Tharsikan <thar.navaratnam@gmail.com>
2021-11-02 14:05:32 +0000

    working solution

---
+++ typewriter.js
@@ -0,0 +1,24 @@
+const typeWriter = (sentence, increment) => {
+  let delay = 0;
+
+  for (const char of sentence) {
+    setTimeout(() => {
+      process.stdout.write(char);
+    }, delay);
+
+    delay += increment;
+  };
+
+  setTimeout(() => {
+    process.stdout.write("\n");
+  }, increment * sentence.length);
+};
+
+const sentence = "Hello, my name is Tharsikan.";
+const increment = 150; 
+
+typeWriter(sentence , increment);
+
+
+
+
